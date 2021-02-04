691d27458bbe235cde7097a98f0326f16a1fad32
Ali Raheem <github@shoryuken.me>
2021-02-04 18:21:50 +0000

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject krapi.clj "0.1.0"
+(defproject krapi.clj "0.1.1"
   :description "Clojure library to access Kraken.com REST API"
   :url "https://github.com/ali-raheem/krapi.clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
